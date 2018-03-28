dbcbb1bc470333a092cfe20ee75bf2093ebc964f
Kevin Kane <kevinthomaskane@gmail.com>
2018-03-27 18:15:21 -0700

    linked to jaws and updated route path

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,10 +1,21 @@
 
+var db = require("../../models")
+
+var counter = 0;
 
 function htmlRoutes(app, path, express){
 
     app.use('/', express.static('images'));
 
     app.get("/", function(req, res) {
+        counter++
+        db.View.update({
+            num: counter
+        },{where: {id: 1}
+        }
+    ).then((data)=>{
+        console.log(data)
+    })
         res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 
+var db = require("./models");
 var htmlRoutes = require("./app/routing/htmlRoutes.js")
 
 var app = express();
@@ -13,6 +14,8 @@ app.use(bodyParser.json());
 
 htmlRoutes(app, path, express)
 
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+db.sequelize.sync({ force: true }).then(function() {
+    app.listen(PORT, function() {
+      console.log("App listening on PORT " + PORT);
+    });
   });
