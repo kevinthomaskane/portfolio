7b815e411ec493c492c43f6960427987cd114d39
Kevin Kane <kevinthomaskane@gmail.com>
2018-03-27 17:38:31 -0700

    functional on local host

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,14 +2,11 @@
 
 function htmlRoutes(app, path, express){
 
-    app.use('/', express.static('public'));
+    app.use('/', express.static('images'));
 
     app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname + "../public/index.html"));
+        res.sendFile(path.join(__dirname, "../public/index.html"));
     });
-
-
-    
 }
 
 module.exports = htmlRoutes;
