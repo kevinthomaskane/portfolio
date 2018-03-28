09b52945a8414abc50b3efb15f9dcdafba20e88e
Travis Kane <travis@Traviss-MacBook-Pro.local>
2018-03-27 21:24:19 -0700

    changed connection details to database_url

--- config/connection.js
+++ config/connection.js
@@ -2,16 +2,28 @@ var mysql = require("mysql");
 
 var connection;
 
-if (process.env.JAWSDB_URL){
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "my_portfolio",
-});
-}
+// if (process.env.JAWSDB_URL){
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//   connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "my_portfolio",
+// });
+// }
+
+if (process.env.DATABASE_URL) {
+    // the application is executed on Heroku ... use the postgres database
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+      dialect:  'postgres',
+      protocol: 'postgres',
+      logging:  true //false
+    });
+  } else {
+    // the application is executed on the local machine
+    sequelize = new Sequelize("postgres:///my_db");
+  }
 
 connection.connect(function(err) {
   if (err) {
