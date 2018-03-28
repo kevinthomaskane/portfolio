e2bcad7a44fc0cef5ed1eff1ed077fdde0f97bf2
Travis Kane <travis@Traviss-MacBook-Pro.local>
2018-03-27 21:29:58 -0700

    another attempt

--- config/connection.js
+++ config/connection.js
@@ -13,17 +13,29 @@ var connection;
 // });
 // }
 
-if (process.env.DATABASE_URL) {
-    // the application is executed on Heroku ... use the postgres database
-    sequelize = new Sequelize(process.env.DATABASE_URL, {
-      dialect:  'postgres',
-      protocol: 'postgres',
-      logging:  true //false
-    });
-  } else {
-    // the application is executed on the local machine
-    sequelize = new Sequelize("postgres:///my_db");
-  }
+var database = process.env.JAWSDB_URL || 'my_portfolio'
+        var sequelize = ""
+
+        if (process.env.JAWSDB_URL) {
+            sequelize = new Sequelize(database)
+        }
+        else {
+            sequelize = new Sequelize(database, 'postgres', '', {
+                dialect: 'postgres'
+            });
+        }
+
+// if (process.env.DATABASE_URL) {
+//     // the application is executed on Heroku ... use the postgres database
+//     sequelize = new Sequelize(process.env.DATABASE_URL, {
+//       dialect:  'postgres',
+//       protocol: 'postgres',
+//       logging:  true //false
+//     });
+//   } else {
+//     // the application is executed on the local machine
+//     sequelize = new Sequelize("postgres:///my_db");
+//   }
 
 connection.connect(function(err) {
   if (err) {
