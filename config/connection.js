1a4d41cdd742243211c94b0d6882291771d82c99
Travis Kane <travis@Traviss-MacBook-Pro.local>
2018-03-28 09:48:40 -0700

    added sequelize info in package

--- config/connection.js
+++ config/connection.js
@@ -2,31 +2,19 @@ var mysql = require("mysql");
 
 var connection;
 
-// if (process.env.JAWSDB_URL){
-//   connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-//   connection = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "",
-//   database: "my_portfolio",
-// });
-// }
+if (process.env.JAWSDB_URL){
+    console.log("jaws")
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    console.log("local")
+  connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "my_portfolio",
+});
+}
 
-
-var sequelize;
-
-if (process.env.JAWSDB_URL) {
-    // the application is executed on Heroku ... use the postgres database
-    sequelize = new Sequelize(process.env.JAWSDB_URL, {
-      dialect:  'mysql',
-      protocol: 'mysql',
-      logging:  true //false
-    });
-  } else {
-    // the application is executed on the local machine
-    sequelize = new Sequelize("mysql:///my_portfolio");
-  }
 
 connection.connect(function(err) {
   if (err) {
