4722a8608aba9694a3e43c860436e07cece66ce3
Travis Kane <travis@Traviss-MacBook-Pro.local>
2018-03-27 20:25:13 -0700

    update

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 
 htmlRoutes(app, path, express)
 
-db.sequelize.sync().then(function() {
+db.sequelize.sync({ force: true }).then(function() {
     app.listen(PORT, function() {
       console.log("App listening on PORT " + PORT);
     });
