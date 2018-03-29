f8da6a15f2ca79f5835eb7b5710c2bd7dfd220ec
Kevin Kane <kevinthomaskane@gmail.com>
2018-03-28 17:29:30 -0700

    update

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ htmlRoutes(app, path, express)
 
 require('dotenv').config();
 
-db.sequelize.sync().then(function() {
+db.sequelize.sync({force: true}).then(function() {
     app.listen(PORT, function() {
       console.log("App listening on PORT " + PORT);
     });
